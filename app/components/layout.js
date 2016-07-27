6442f2772c4b37fa9b606079193c43bee789acd4
Roman <alerdon@gmail.com>
2016-07-27 17:48:56 +0300

    Continue creating layout

--- app/components/layout.js
+++ app/components/layout.js
@@ -5,31 +5,22 @@ import {Grid, Col, Row} from 'react-bootstrap';
 const Layout = () => {
   return (
     <Grid>
-      <Row className="show-grid">
-        <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-        <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-      </Row>
+      <Row>
+        <Col md={3}>
+          <Row>Photo</Row>
+          <Row>Menu</Row>
+        </Col>
 
-      <Row className="show-grid">
-        <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-        <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-        <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
-      </Row>
-
-      <Row className="show-grid">
-        <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
-      </Row>
-
-      <Row className="show-grid">
-        <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
-        <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+        <Col md={9}>
+          Content
+        </Col>
       </Row>
     </Grid>
   );
 };
 
 
-export default Layout
+export default Layout;
 
 
 
