35ba88bbd2adf8f3374d95bedffcb550562663bc
Roman Vitruk <alerdon@gmail.com>
2016-07-28 00:47:37 +0300

    Started redux saga implementation

---
+++ app/sagas.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import {takeEvery} from 'redux-saga';
+import {call, put} from 'redux-saga/effects';
+
+function* fetchUser(action) {
+  try{
+
+    const user = yield call()
+  }
+
+}
--- app/store.js
+++ app/store.js
@@ -1,8 +1,12 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import VkApp from './reducers';
+import sagas from './sagas';
 import {Map} from 'immutable';
 
 
-const store = createStore(VkApp, Map({clicks: 0}));
+const sagaMiddleWare = createSagaMiddleware();
+const store = createStore(VkApp, Map({clicks: 0}), applyMiddleware(sagaMiddleWare));
+sagaMiddleWare.run(sagas);
 
 export default store;
