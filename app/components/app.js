7d658190f8d96c0549bf24ea0d560579f406e005
Roman <alerdon@gmail.com>
2016-07-27 12:38:26 +0300

    Added live reload

--- app/components/app.js
+++ app/components/app.js
@@ -5,7 +5,7 @@ import {Button, Panel} from 'react-bootstrap';
 const App = ({onClick, clicks}) => {
     return (
         <Panel header={`clicks: ${clicks}`}>
-            <Button bsStyle="danger" onClick={onClick}>Success</Button>
+            <Button bsStyle="danger" onClick={onClick}> Success </Button>
         </Panel>
     );
 }
@@ -16,7 +16,7 @@ const App = ({onClick, clicks}) => {
 
 App.propTypes = {
     onClick: PropTypes.func.isRequired,
-    clicks: PropTypes.number.isRequired,
+    clicks: PropTypes.number.isRequired
 };
 
 export default App
