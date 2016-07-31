6024bd01c21ce6b9a05f023ea96cdb652463a0db
Roman Vitruk <alerdon@gmail.com>
2016-07-31 23:37:05 +0300

    Get user gaga tests

--- app/sagas/tests/sagas_tests.js
+++ app/sagas/tests/sagas_tests.js
@@ -4,19 +4,21 @@ import {get_user} from '../index';
 import {call, put} from 'redux-saga/effects';
 import {get as getUser} from '../../lib/vk/api/user';
 
-describe('Sagas', () => {
-  describe('* get_user()', () => {
-    it('resolves get_user', () => {
+describe('Sagas', function () {
+  describe('* get_user()', function () {
+    beforeEach(function () {
       const user_ids = 210700286,
         fields = ['photo_100', 'sex'],
         query_params = {user_ids, fields};
 
-      const action = {query_params};
-      const generator = get_user(action);
+      this.action = {query_params};
+      this.generator = get_user(this.action);
 
-      expect(generator.next().value)
-        .toEqual(call(getUser, action.query_params));
+      expect(this.generator.next().value)
+        .toEqual(call(getUser, this.action.query_params));
+    });
 
+    it('resolves get_user', function () {
       const user = {
         "response": [{
           "uid": 210700286,
@@ -27,10 +29,20 @@ describe('Sagas', () => {
         }]
       };
 
-      expect(generator.next(user).value)
+      expect(this.generator.next(user).value)
         .toEqual(put({type: 'GET_USER_RESOLVED', user}));
-
-      expect(generator.next().done).toEqual(true);
     });
+
+    it('rejects get_user', function () {
+      const error = new Error('error happened');
+
+      expect(this.generator.throw(error).value)
+        .toEqual(put({type: 'GET_USER_REJECTED', error}));
+
+    });
+
+    afterEach(function () {
+      expect(this.generator.next().done).toEqual(true);
+    })
   });
 });
