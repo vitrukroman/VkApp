2c67f5a8525ad314f517a570bf2dd5563c81f150
Roman <alerdon@gmail.com>
2016-08-03 10:28:32 +0300

    Continue search form

--- app/components/search.js
+++ app/components/search.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
-import {FormGroup, FormControl, Button} from 'react-bootstrap';
+import {
+  FormGroup, FormControl, Button, Form,
+  Grid, Row, Col, ControlLabel
+} from 'react-bootstrap';
 
 class Search extends Component {
   render() {
     return (
-      <FormGroup controlId='searchQuery'>
-        <FormControl inline/>
-        <Button bsStyle="primary" inline>Пошук</Button>
-      </FormGroup>
+      <Form>
+        <FormGroup controlId='searchQuery' inline>
+          <FormControl inline/>
+          <Button bsStyle="primary">Пошук</Button>
+            <Row>
+              <Col md={6}>Знайдені</Col>
+              <Col md={6}>
+
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    Email
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl type="email" placeholder="Email" />
+                  </Col>
+                </FormGroup>
+
+                <FormGroup controlId="formHorizontalPassword">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    Password
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl type="password" placeholder="Password" />
+                  </Col>
+                </FormGroup>
+              </Col>
+            </Row>
+        </FormGroup>
+
+      </Form>
     );
   }
 }
